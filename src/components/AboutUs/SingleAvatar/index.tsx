0cd66241c75f3592e6ac8c5722ffac78e3ad574d
Sandro Saralidze <saralidze.sandro@kiu.edu.ge>
2025-03-30 03:55:31 +0400

    round

--- src/components/AboutUs/SingleAvatar/index.tsx
+++ src/components/AboutUs/SingleAvatar/index.tsx
@@ -10,7 +10,7 @@ interface ProfileCardProps {
 const ProfileCard: React.FC<ProfileCardProps> = ({ avatar, name, position, description }) => {
   return (
     <div className="flex flex-col items-center p-4 rounded-2xl shadow-lg w-96 text-center">
-      <img src={avatar} alt={name} className="srounded-full mb-3 h-36 w-36" />
+      <img src={avatar} alt={name} className="rounded-full mb-3 h-36 w-36" />
       <h2 className="text-lg font-semibold text-white">{name}</h2>
       <p className="text-sm text-gray-500">{position}</p>
       <p className="text-xs text-gray-600 mt-2">{description}</p>
